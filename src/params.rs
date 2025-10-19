2bf4be641394dcee53db3a5574ff91d813e67747
Tony Kan <tonykan@caltech.edu>
2025-10-19 14:16:44 -0700

    feat(params): Enhance element map deserialization with atomic number parsing

--- src/params.rs
+++ src/params.rs
@@ -48,3 +48,35 @@ impl Default for Parameters {
         Self::new()
     }
 }
+
+fn deserialize_element_map<'de, D>(deserializer: D) -> Result<HashMap<u8, ElementData>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    struct ElementMapVisitor;
+
+    impl<'de> Visitor<'de> for ElementMapVisitor {
+        type Value = HashMap<u8, ElementData>;
+
+        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+            formatter.write_str("a map from atomic number or symbol to element data")
+        }
+
+        fn visit_map<M>(self, mut map: M) -> Result<Self::Value, M::Error>
+        where
+            M: MapAccess<'de>,
+        {
+            let mut elements = HashMap::with_capacity(map.size_hint().unwrap_or(0));
+            while let Some((key, value)) = map.next_entry::<String, ElementData>()? {
+                let atomic_number = key.parse::<u8>().or_else(|_| {
+                    element_symbol_to_atomic_number(&key)
+                        .ok_or_else(|| de::Error::custom(format!("invalid element key: '{}'", key)))
+                })?;
+                elements.insert(atomic_number, value);
+            }
+            Ok(elements)
+        }
+    }
+
+    deserializer.deserialize_map(ElementMapVisitor)
+}
